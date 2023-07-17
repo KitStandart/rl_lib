82c1ddef90298fb9e5390f4d571c720f5bfcb273
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-17 10:42:13 +0300

    Create base_model.py

---
+++ src/models/base_model.py
@@ -0,0 +1,51 @@
+import abc
+
+
+class Model(abc.ABC):
+  """Абстрактный базовый класс,
+  представляющий общий интерфейс для всех алгоритмов и моделей в RL-Lib.
+
+  Model определяет общие методы для ввода, вывода и базовых вычислений,
+  которые должны быть реализованы в каждом конкретном алгоритме или модели.
+
+  Этот класс служит в качестве основы для всех других классов в RL-Lib
+  и обеспечивает единый интерфейс для работы с различными моделями.
+  """
+
+  def __init__(self):
+    pass
+
+  @property
+  @abc.abstractmethod
+  def input_spec(self) -> tuple:
+    """Возвращает кортеж размера входных данных Модели"""
+
+  @property
+  @abc.abstractmethod
+  def output_spec(self) -> tuple:
+    """Возвращает кортеж размера выходных данных Модели"""
+
+  @abc.abstractmethod
+  def initial_state(self) -> None:
+    """Инициализирует внутреннее состояние реккурентной Модели"""
+
+  @abc.abstractmethod
+  def _update_next_state(self) -> None:
+    """Обновляет внутреннее состояние реккурентной Модели"""
+
+class ModelIO(abc.ABC):
+  def __init__(self, config):
+    self._config = config
+
+  @abc.abstractmethod
+  def config(self):
+    """Возвращает конфигурацию алгоритма"""
+    return
+
+  @abc.abstractmethod
+  def save(self):
+    """Сохраняет модель в директории"""
+
+  @abc.abstractmethod
+  def load(self):
+    """Загружает модель из директории"""
