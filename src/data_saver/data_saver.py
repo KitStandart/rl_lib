7edf7269a8402dad01ac49146ad37d70edca533a
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-17 16:13:10 +0300

    Update data_saver.py

--- src/data_saver/data_saver.py
+++ src/data_saver/data_saver.py
@@ -1,13 +1,15 @@
 import os
 
 class Saver:
-  """Хранит в себе пути сохранения этапа обучения и путь резервного копирования
+  """Хранит в себе пути сохранения этапа обучения и путь резервного копирования.
+  При инициализации создает папки для сохранения и резервного копирования.
   Args:
     name: str. Необязательно, название алгоритма
     path: str. Путь сохранения
     copy_path: str. Путь резервного копирования
   """
-  def __init__(self, name="unkown", path="", copy_path=""):
+  def __init__(self, name="unkown", path="", copy_path="", **kwargs):
+    super().__init__(**kwargs)
     self.original_path = os.getcwd()
     self.name = name
     self.copy_path = copy_path
@@ -39,8 +41,11 @@ class Saver:
     assert type(self.copy_path) == str, "Неверный тип аргумента"
     assert type(self.name) == str, "Неверный тип аргумента"
 
+  @property
   def get_save_path(self):
     return self.path
   
+  @property
   def get_copy_path(self):
-    return self.copy_path
+    if self.copy_path != "/models/" + self.name +'/': return self.copy_path 
+    else: return "Path is not defined"
