8abbd219abead11859dba0cafa9d780d8b594796
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-18 16:34:55 +0300

    Create base_explore.py

---
+++ rl_lib/src/explore_env/base_explore.py
@@ -0,0 +1,29 @@
+import abc
+
+class Base_Explore(abc.ABC):
+  """Абстрактный класс представляющий общий интерфейс для всех классов исследования
+  нейронной сетью среды обучения
+  
+  """
+  def __init__():
+    pass
+  
+  @abc.abstractmethod
+  def reset(self, ) -> None:
+    """Выполняет внутренний сброс"""
+  
+  @abc.abstractmethod
+  def save(self, path) -> None:
+    """Сохраняет какие либо внутренние переменные"""
+    
+  @abc.abstractmethod
+  def load(self, path) -> None:
+    """Загружает какие либо внутренние переменные"""
+
+  @abc.abstractmethod
+  def __call__(self, action) -> int:
+    """Возвращает действие в соответствии с стратегией исследования"""
+
+  @abc.abstractmethod
+  def test(self, action) -> int:
+    """Возвращает действие в соответствии с стратегией тестирования"""
