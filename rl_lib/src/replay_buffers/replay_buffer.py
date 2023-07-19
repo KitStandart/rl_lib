860a1a8e6d66c16de3d62015e14cb2e7bcb7d8a8
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 15:42:51 +0300

    Update replay_buffer.py

--- rl_lib/src/replay_buffers/replay_buffer.py
+++ rl_lib/src/replay_buffers/replay_buffer.py
@@ -20,31 +20,36 @@ class ReplayBuffer:
     """
   
   def __init__(self, **kwargs):
+    self._config = kwargs
     if kwargs.get("priority", 0) :
       if kwargs.get("recurrent", 0):Prioritized_Replay_Recurrent_Buffer(**kwargs)
       else: self.buffer = Prioritized_Replay_Buffer(**kwargs)
     else:
       if kwargs.get("recurrent", 0):Random_Recurrent_Buffer(**kwargs)
       else: self.buffer = Random_Buffer(**kwargs)
-
-  @property
-  def name(self):
-    return self.buffer.name
+        
+  def add(self, *args):
+    self.buffer.add(*args)
+    
+  @propety
+  def config(self):
+    return self._config
     
   def clear(self):
     self.buffer.clear()
     
-  def add(self, *args):
-    self.buffer.add(*args)
+  def load(self, *args):
+    self.buffer.load(*args)   
+    
+  @property
+  def name(self):
+    return self.buffer.name
 
   def sample(self, *args):
     return self.buffer.sample(*args)
 
-  def update_priorities(self, *args):
-    self.buffer.update_priorities(*args)
-
   def save(self, *args):
     self.buffer.save(*args)
     
-  def load(self, *args):
-    self.buffer.load(*args)
+  def update_priorities(self, *args):
+    self.buffer.update_priorities(*args)
