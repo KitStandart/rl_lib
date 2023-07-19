b16cbc3ac1ff5c85a1f2fb15f98e4d5c5a9ee220
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 09:02:46 +0300

    Update epsilon_greedy.py

--- rl_lib/src/explore_env/epsilon_greedy.py
+++ rl_lib/src/explore_env/epsilon_greedy.py
@@ -1,15 +1,30 @@
 import numpy as np 
-from saving_data.utils import save_data, load_data
+from tensorflow.math import argmax
+from tensorflow.dtypes import int32
+
+from ..data_saver.utils import save_data, load_data
 from .base_explore import Base_Explore
 
 class Epsilon_Greedy(Base_Explore):
-    '''Epsilon greedy explore strategy'''
-    def __init__(self, **kwargs):
-        self.eps_desay_steps = kwargs.get("eps_decay_steps", 1e6)
-        self.eps_min = kwargs.get("eps_min", 1e-1)
-        self.eps_max = kwargs.get("eps_max", 1.)
-        self.eps_test = kwargs.get("eps_test", 1e-3)
-        self.action_space = kwargs.get("action_space", 2)
+    """Эпсилон-жадная стратегия исследования
+
+    Kwargs:
+        eps_decay_steps: int, Количество внутренних шагов исследований до установки минимального эпсилон
+        eps_max: float, Максимальный эпсилон
+        eps_min: float, Минимальный эпсилон
+        eps_test: float, Тестовый эпсилон
+        action_spase: int, Размер пространтства действий
+        axis: int, Ось вычислений
+    """
+    def __init__(self, eps_decay_steps=1e6, eps_max=1.0, eps_min=1e-1, eps_test=1e-3, action_space=None, axis=-1):
+        self.eps_desay_steps = eps_decay_steps
+        self.eps_min = eps_max
+        self.eps_max = eps_min
+        self.eps_test = eps_test
+        assert type(action_space) == int, "Пространство действий должно быть int"
+        self.action_space = action_space 
+        self.axis = kwargs.get("action_space", 2)
+        self.name = "epsilon_greedy_strategy"
         self.reset()
 
     def reset(self, ):
@@ -17,22 +32,19 @@ class Epsilon_Greedy(Base_Explore):
         self.eps = self.eps_max
     
     def save(self, path):
-        save_data(path ,{
-                'count': self.count,
-                    })
+        save_data(path+self.name, self.__dict__)
 
     def load(self, path):
-        data = load_data(path)
-        self.count = data['count']
+        self.__dict__ = load_data(path+self.name)
 
-    def __call__(self, action):
+    def __call__(self, Q):
         self.eps = max(self.eps_min, self.eps_max - (self.eps_max-self.eps_min) * self.count/self.eps_desay_steps)
         self.count += 1
-        return self.get_action(self.eps, action)
+        return self.get_action(self.eps, Q)
 
-    def test(self, action):
-        return self.get_action(self.eps_test, action)
+    def test(self, Q):
+        return self.get_action(self.eps_test, Q)
 
     def get_action(self, eps, action):
         if np.random.random() < eps: return  np.random.randint(self.action_space)
-        else: return action
+        else: return argmax(Q, axis=self.axis, output_type=int32)
