dee291890fc20975e3ae30ae7108822c2d51ac5d
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 11:56:00 +0300

    Update optimizers.py

--- rl_lib/src/optimizers/optimizers.py
+++ rl_lib/src/optimizers/optimizers.py
@@ -1,7 +1,7 @@
 import tensorflow.keras.optimizers as optimizers
 import tensorflow_addons as tfa
 
-def set_optimizer(self, optimizer: str = "adam", optimizer_params: dict = {}, cutom_optimizer: object = None) -> object:
+def get_optimizer(self, optimizer_name: str = "adam", optimizer_params: dict = {}, cutom_optimizer: object = None) -> object:
   """Возврщает настроенный оптимизатор.
   Доступные оптимизаторы tensorflow:
     Adam
@@ -13,17 +13,17 @@ def set_optimizer(self, optimizer: str = "adam", optimizer_params: dict = {}, cu
     optimizer_params: dict: Параметры оптимизатора
     cutom_optimizer: object: Класс кастомного потимизатора
   """
-  if optimizer.lower() == 'adam':
+  if optimizer_name.lower() == 'adam':
     return optimizers.Adam(**optimizer_params)
     
-  elif optimizer.lower() == 'lamb':
+  elif optimizer_name.lower() == 'lamb':
     return tfa.optimizers.LAMB(**optimizer_params)
 
-  elif optimizer.lower() == 'cutom' and type(cutom_optimizer) != None:
+  elif optimizer_name.lower() == 'cutom' and type(cutom_optimizer) != None:
       return cutom_optimizer(**optimizer_params)
 
-  elif optimizer.lower() == 'adadelta':
+  elif optimizer_name.lower() == 'adadelta':
     return optimizers.Adam(**optimizer_params)
 
-  elif optimizer.lower() == 'rmsprop':
+  elif optimizer_name.lower() == 'rmsprop':
     return optimizers.Adam(**optimizer_params)
