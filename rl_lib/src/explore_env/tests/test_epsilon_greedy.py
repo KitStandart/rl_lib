5ce9148efc5a155177d336cf68492f207a732e8b
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 09:09:55 +0300

    Create test_epsilon_greedy.py

---
+++ rl_lib/src/explore_env/tests/test_epsilon_greedy.py
@@ -0,0 +1,19 @@
+from ..epsilon_greedy import Epsilon_Greedy
+
+class Test_Epsilon_Greedy:
+  def __init__(config):
+    self.strategy = Epsilon_Gredy(**config)
+    self.config = config
+
+  def test_reset(self):
+    pass
+
+  def test_save(self):
+    pass
+    
+  def test_call(self):
+    pass
+    
+  def test_test(self):
+    pass
+    
