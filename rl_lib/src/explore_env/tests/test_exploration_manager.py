341032bfca6bbb915f117dc981dadda3b3d38dbc
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 09:11:17 +0300

    Create test_exploration_manager.py

---
+++ rl_lib/src/explore_env/tests/test_exploration_manager.py
@@ -0,0 +1,19 @@
+from ..exploration_manger import Exploration_Manger
+
+class Test_Epsilon_Greedy:
+  def __init__(config):
+    self.strategy = Exploration_Manger(**config)
+    self.config = config
+
+  def test_reset(self):
+    pass
+
+  def test_save(self):
+    pass
+    
+  def test_call(self):
+    pass
+    
+  def test_test(self):
+    pass
+    
