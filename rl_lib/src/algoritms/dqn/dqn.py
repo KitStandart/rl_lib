c877fe829901eefc1afd547a729a2fc51ec1c7af
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-19 11:10:22 +0300

    Update and rename dqn.py to dqn.py

--- rl_lib/src/algoritms/dqn/dqn.py
+++ rl_lib/src/algoritms/dqn/dqn.py
@@ -1,7 +1,7 @@
 from tensorflow.keras import layers
 
-from ..models.base_model import *
-from ..algoritms.base_algo import *
+from ..models.model import Model
+from ..algoritms.base_algo import Base_Algo
 
 class DQN_Model(ModelNN, ModelIO, Model):
   def __init__(self, config = None, model = None, **kwargs):
@@ -16,37 +16,11 @@ class DQN_Model(ModelNN, ModelIO, Model):
   def initial_state(self):
     pass
 
-  def input_spec(self):
-    return self.model.layers[0].input_shape[0]
-
-  def output_spec(self):
-    return self.model.layers[-1].input_shape[0]
-
-  def load(self):
-    """Args: ModelIO.path, ModelNN.name"""
-    self.model = tf.keras.models.load_model(self.path+self.name)
-
-  def save(self):
-    """Args: ModelIO.path, ModelNN.name"""
-    self.model.save(self.path+self.name)
-
-class DQN_Algo(DQN_Model, Base_Algo):
+class DQN_Algo(SampleQ):
   def __init__(self, config, model):
     super().__init__(model = model, config = config)
     self.action_model = DQN_Model(model = model, config = config, name = "DQN_action" + config.get("name", ""))
     self.target_model = DQN_Model(model = model, config = config, name = "DQN_target" + config.get("name", ""))
-  
-  def _initial_model(self):
-    if len(self._config["input_shape"]) <= 1:
-        return self.create_model(self._config["input_shape"], self._config["action_space"])
-    else:
-      return self.create_model_with_conv(self._config["input_shape"], self._config["action_space"])
-  
-  def initial_model(self):
-      model = self._initial_model()
-      self.action_model.set_new_model(model, tf.keras.optimizers.Adam())
-      self.target_model.set_new_model(model, tf.keras.optimizers.Adam())
-      self.target_model.set_weights(self.action_model.get_weights())
 
   @staticmethod
   def create_model(input_shape: tuple, action_space: int) -> tf.keras.Model:
