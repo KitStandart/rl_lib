c199227c12b0a516f8eab6685379e3f4512e17fc
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-20 14:45:29 +0300

    Update simple_q.py

--- rl_lib/src/algoritms/simple_q.py
+++ rl_lib/src/algoritms/simple_q.py
@@ -18,23 +18,42 @@ class SimpleQ(Base_Algo):
     self.discount_factor = self.config['model_config']['discount_factor']
     self.n_step = self.config['model_config']['n_step']
 
-    self.batch_size = config.get("batch_size", 32)
-    self.double_network = config.get("double_network", True)
-    self.priority = config.get("priority", False)
-    self.tau = config.get("tau", 0.01)
+    self.batch_size = config.get("batch_size")
+    self.double_network = config.get("double_network")
+    self.priority = config.get("priority")
+    self.tau = config.get("tau")
 
     self.batch_dims = -1
     self.ind_axis = -1
 
     self.path = self.action_model.path
-
+  
   def add(self, data, priority = None):
     """Добавляет переходы в буфер"""
     self.buffer.add(data, priority)
-  
+
+  def calculate_double_q(self, **kwargs):
+    Qaction = self.action_model(kwargs['next_state'])
+    Qtarget = self.target_model(kwargs['next_state'])
+    Qaction = Qaction[0] if isinstance(Qaction, list) else Qaction
+    Qtarget = Qtarget[0] if isinstance(Qtarget, list) else Qtarget
+    if kwargs["p_double"] < 0.5 : Qtarget = self.get_best_action(Qtarget, Qaction)
+    else: Qtarget = self.get_best_action(Qaction, Qtarget)
+    return Qtarget
+    
+  def calculate_gradients(self, **batch):
+    batch = self.choice_model_for_double_calculates(**batch)
+    return self.action_model.calculate_gradients(**batch) if batch['p_double'] > 0.5 else self.target_model.update_weights.calculate_gradients(**batch)
+    
   def calculate_new_best_action(self) -> tf.Tensor:
     """Вычислеят новое лучшее действие для получения таргета"""
-    pass
+    if self.double_network:
+        Qtarget = self.calculate_double_q(**kwargs)
+    else:
+        Qtarget = self.target_model(kwargs['next_state'])
+        Qtarget = Qtarget[0] if isinstance(Qtarget, list) else Qtarget
+        Qtarget = self.get_best_action(Qtarget, Qtarget)
+    return Qtarget
     
   @tf.function(reduce_retracing=True,
                  jit_compile=False,
@@ -48,9 +67,19 @@ class SimpleQ(Base_Algo):
         Qtarget = Qtarget[:, kwargs.get('recurrent_skip', 10):]
     return Qtarget
 
+  def choice_model_for_double_calculates(self, **batch):
+    batch['p_double'] = tf.random.uniform((1,), minval = 0.0, maxval = 1.0) if self.double_network else 1.
+    batch['Qtarget'] = self.calculate_Qtarget(**batch)
+    return batch
+    
   def get_batch(self):
     """Получает батч из буфера"""
     return self.buffer.sample(self.batch_size)
+
+  def get_batch_and_td_error(self):
+    batch = self.get_batch()
+    td_error = self.calculate_gradients(**batch)['td_error']
+    return {'td_error': td_error.numpy(), 'batch': batch}
     
   def get_best_action(self, Qaction, Qtarget):
     ind = tf.argmax(Qaction, axis=self.ind_axis)
@@ -59,7 +88,8 @@ class SimpleQ(Base_Algo):
     
   def get_gradients(self) -> tf.Tensor:
     """Вычисляет градиенты и возвращает их"""
-    pass
+    batch = self.get_batch()
+    return self.calculate_gradients(**batch)['gradients']
     
   def load(self, path) -> None:
     """Загружает алгоритм"""
@@ -76,7 +106,9 @@ class SimpleQ(Base_Algo):
     
   def _train_step(self) -> dict:
     """Вспомогательная train_step"""
-
+    batch = self.choice_model_for_double_calculates(**batch)
+    return self.action_model.update_weights(**batch) if batch['p_double'] > 0.5 else self.target_model.update_weights(**batch)
+    
   def train_step(self) -> np.array:
     """Вычисляет полный обучающий шаг"""
     batch = self.get_batch()
@@ -99,6 +131,8 @@ class SimpleQ(Base_Algo):
   def summary(self) -> None:
     """Выводит архитектуру модели"""
     self.action_model.summary()
+
+
 
 def link_data_inside_the_config(config):
   discount_factor = config['model_config']['discount_factor']
