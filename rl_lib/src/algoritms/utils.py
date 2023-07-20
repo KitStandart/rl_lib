98eb01a0696ec910f9466b64a97060d4917a5bf1
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-20 08:59:40 +0300

    Update utils.py

--- rl_lib/src/algoritms/utils.py
+++ rl_lib/src/algoritms/utils.py
@@ -1,6 +1,11 @@
-
-def update_config(config, new_data):
-  """Обвновляет конфигурацию по умолчанию"""
+def update_config(config: dict, new_data: dict) -> None:
+  """Обвновляет конфигурацию по умолчанию
+  Args:
+    config: dict: Конфигурация, которую надо обновить
+    new_data: dict: Конфигурация с новыми данными
+  Returns:
+    None
+  """
   for key, value in new_data.items():
     if isinstance(value, dict) and key in config and isinstance(config[key], dict):
       update_config(config[key], value)
