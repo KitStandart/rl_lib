95f51cc6e20f2b6ca500c7743334fbcb23fd9f6c
KitStandart <104554691+KitStandart@users.noreply.github.com>
2023-07-21 12:51:30 +0300

    Update test_saver.py

--- rl_lib/src/data_saver/tests/test_saver.py
+++ rl_lib/src/data_saver/tests/test_saver.py
@@ -1,18 +1,19 @@
-from .saver import Saver
+from ..saver import Saver
 from rl_lib.rl_lib.src.algoritms.tests.test_simpe_q import get_directory_structure, compare_directory_structures
 
 class Test_Saver:
   def __init__(self, **kwargs):
     self.saver = Saver(**kwargs)
 
-def test_init(self, path, copy_path):
-  self.check_structure(self.saver.path, path)
-  self.check_structure(self.saver.copy_path, copy_path)
-  print("Тест пройден успешно")
+  def test_init(self, path, copy_path):
+    self.check_structure(self.saver.path, path)
+    self.check_structure(self.saver.copy_path, copy_path)
+    print("Тест пройден успешно")
 
 
-def check_structure(self, real_path, corrrect_path):
-  real_structure = get_directory_strucrure(real_path)
-  assert real_path != corrrect_path, "Пути не совпадают"
-  correct_structure = {corrrect_path: {self.name: None}}
-  assert compare_directory_structures(real_structure, correct_structure), "Каталоги разные"
+  def check_structure(self, real_path, corrrect_path):
+    real_structure = get_directory_structure(real_path)
+    assert real_path == corrrect_path, "Пути не совпадают"
+    if corrrect_path != "":
+      correct_structure = {corrrect_path.replace("/", ""): {"":{}, self.saver.name: {}}}
+      assert compare_directory_structures(real_structure, correct_structure), "Каталоги разные"
